6669ef8cc8f1526a3c776ba482fc6afff808b251
Δημητρης Θεοδωροπουλος <jim@Demetress-MacBook-Air.local>
2017-10-09 20:41:24 +0300

    feat: Start adding redux

---
+++ actions/index.js
@@ -0,0 +1,6 @@
+import { getDecks, getDeck, saveDeckTitle, addCardToDeck } from '../utils/api'
+
+export const GET_DECKS = 'GET_DECKS'
+export const GET_DECK = 'GET_DECK'
+export const SAVE_DECK_TITLE = 'SAVE_DECK_TITLE'
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK'
---
+++ utils/api.js
@@ -0,0 +1,19 @@
+import { AsyncStorage } from 'react-native'
+
+const DECK_QUIZ_APP = 'DECK_QUIZ_APP'
+
+export function getDecks () {
+  return null
+}
+
+export function getDeck (title) {
+  return null
+}
+
+export function saveDeckTitle (title) {
+  return null
+}
+
+export function addCardToDeck (title, card) {
+  return null
+}
