aceddadb71c6798e7b4cebbfe8125f218bf2938f
Dimitrios_Theodoropoulos <theodoropoulos.dimitris2@gmail.com>
2017-10-13 00:33:35 +0300

    feat: Add animation to display number of cards to the Deck view

--- components/Deck.js
+++ components/Deck.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import styled from 'styled-components/native'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
+import {Animated, StyleSheet} from 'react-native'
 
 const MainView = styled.View`
   flex: 1;
@@ -11,11 +12,6 @@ const MainView = styled.View`
 const DeckTitle = styled.Text`
   margin-top: 30%;
   font-size: 30;
-`
-
-const NumberOfCards = styled.Text`
-  font-size: 20;
-  color: gray;
 `
 
 const AddCard = styled.TouchableOpacity`
@@ -47,37 +43,41 @@ const StartQuizText = styled.Text`
   font-size: 20;
   color: white;
 `
-//TODO add animation??
+
 class Deck extends Component {
     state = {
         quizDisabled: false,
+        opacity: new Animated.Value(0)
     }
 
-    componentDidMount () {
-        const { title } = this.props.navigation.state.params
+    componentDidMount() {
+        const {title} = this.props.navigation.state.params
         const numberOfCards = this.props.decks[title].numberOfCards
+        const {opacity} = this.state
         if (numberOfCards === 0) {
-            this.setState({ quizDisabled: true })
-        }else {
-            this.setState({ quizDisabled: false })
+            this.setState({quizDisabled: true})
+        } else {
+            this.setState({quizDisabled: false})
         }
+        Animated.timing(opacity, {toValue: 1, duration: 1500}).start()
     }
 
     handleQuiz = (title, numberOfCards) => {
         if (this.state.quizDisabled === true) {
             alert('Would you really test your knowledge with 0 questions??? Please add one first...')
-        }else {
+        } else {
             this.props.navigation.navigate('Quiz', {title: title, numberOfCards: numberOfCards})
         }
     }
 
     render() {
-        const { title } = this.props.navigation.state.params
+        const {title} = this.props.navigation.state.params
         const numberOfCards = this.props.decks[title].numberOfCards
+        const {opacity} = this.state
         return (
             <MainView>
                 <DeckTitle>{title}</DeckTitle>
-                <NumberOfCards>{numberOfCards} cards</NumberOfCards>
+                <Animated.Text style={[styles.text, {opacity}]}>{numberOfCards} cards</Animated.Text>
                 <AddCard onPress={() => this.props.navigation.navigate('AddCard', {title: title})}>
                     <AddCardText>Add Card</AddCardText>
                 </AddCard>
@@ -89,12 +89,19 @@ class Deck extends Component {
     }
 }
 
-function mapStateToProps (decks) {
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        color: 'gray'
+    }
+})
+
+function mapStateToProps(decks) {
     return {
         decks,
     }
 }
 
-export default connect (
+export default connect(
     mapStateToProps
 )(Deck)
